3926db4696e49f65dcc028c6e28c9fd21f692314
a-taghva <taghvali99@gmail.com>
2021-05-05 13:13:43 -0400

    configure server to work in prod env

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const db = require('./config/connection')
 
 const { authMiddleware } = require('./utils/auth');
 
+const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 // create a new apollo server and pass in our schema data
@@ -21,6 +23,14 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 db.once('open', () => {
   app.listen(PORT, () => {
