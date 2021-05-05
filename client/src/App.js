5d3cdebc0a607a55f82b42ce3d6859694ecb063e
a-taghva <taghvali99@gmail.com>
2021-05-04 23:18:36 -0400

    ApolloProvider and ApolloClient

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
 
+// special type of react component, use to provide data to all of other components
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import Home from './pages/Home';
 
+// establish a new connection to the GraphQL server using Apollo
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql'
+})
+
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
-      <Header />
-      <div className='container'>
-        <Home />
+    <ApolloProvider client={client}>
+      <div className='flex-column justify-flex-start min-100-vh'>
+        <Header />
+        <div className='container'>
+          <Home />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </ApolloProvider>
   );
 }
 
