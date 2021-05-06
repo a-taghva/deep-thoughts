8f67fc7484c3462c4c7a7ac1ab082d3d1112c275
a-taghva <taghvali99@gmail.com>
2021-05-06 18:01:18 -0400

    modify friendList

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -33,8 +33,8 @@ const Profile = () => {
       <div className="flex-row justify-space-between mb-3">
         <div className="col-12 mb-3 col-lg-8">
           <ThoughtList thoughts={user.thoughts} title={`${user.username}'s thoughts...`} />
-          <FriendList username={user.username} friendCount={user.friendCount} friends={user.friends} />
         </div>
+
         <div className="col-12 col-lg-3 mb-3">
           <FriendList
             username={user.username}
