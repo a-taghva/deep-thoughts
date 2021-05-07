adfec49478eef685d62e723355a076474f1c9ff6
a-taghva <taghvali99@gmail.com>
2021-05-06 23:28:01 -0400

    thoughtText and characterCount

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,14 +1,28 @@
 import React, { useState } from 'react';
 
 const ThoughtForm = () => {
+  const [thoughtText, setText] = useState('');
+  const [characterCount, setCharacterCount] = useState('');
+
+  const handleChange = e => {
+    const l = e.target.value.length;
+
+    if (l <= 280) {
+      setText(e.target.value);
+      setCharacterCount(e.target.value.length);
+    }
+  }
+
   return (
     <div>
-      <p className="m-0">
-        Character Count: 0/280
+      <p className={`m-0 ${characterCount === 280 ? 'text-error' : '' }`}>
+        Character Count: {characterCount}/280
       </p>
       <form className="flex-row justify-center justify-space-between-md align-stretch">
         <textarea
           placeholder="Here's a new thought..."
+          value={thoughtText}
+          onChange={handleChange}
           className="form-input col-12 col-md-9"
         ></textarea>
         <button className="btn col-12 col-md-3" type="submit">
