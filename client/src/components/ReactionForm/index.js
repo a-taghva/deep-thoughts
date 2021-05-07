90a0ac4501ed4eec58a96466c7d4c2de28f9049b
a-taghva <taghvali99@gmail.com>
2021-05-07 00:16:10 -0400

    handleChange handleFormSubmit reactionForm

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -1,13 +1,37 @@
 import React, { useState } from 'react';
 
 const ReactionForm = ({ thoughtId }) => {
+  const [reactionBody, setBody] = useState('');
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const handleChange = e => {
+    const l = e.target.value.length;
+
+    if (l <= 280) {
+      setBody(e.target.value);
+      setCharacterCount(l);
+    }
+  };
+
+  const handleFormSubmit = async e => {
+    e.preventDefault();
+
+    setBody('');
+    setCharacterCount(0);
+  };
+
   return (
     <div>
-      <p className="m-0">
-        Character Count: 0/280
+      <p className={`m-0 ${characterCount === 280 ? 'text-error' : '' }`}>
+        Character Count: {characterCount}/280
       </p>
-      <form className="flex-row justify-center justify-space-between-md align-stretch">
+      <form 
+        className="flex-row justify-center justify-space-between-md align-stretch"
+        onSubmit={handleFormSubmit}
+      >
         <textarea
+          value={reactionBody}
+          onChange={handleChange}
           placeholder="Leave a reaction to this thought..."
           className="form-input col-12 col-md-9"
         ></textarea>
